919ec46881396821dd97ca35297ce8893ad5828c
Vasily Kurichev <dekill@yandex.ru>
2021-02-08 16:17:41 +0300

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -6,19 +6,19 @@ const dataPath = path.join(__dirname, '..', 'data', 'users.json');
 const getUsers = (req, res) => {
   return getDataFromFile(dataPath)
     .then(users => res.status(200).send(users))
-    .catch(err => res.status(404).send(err));
+    .catch(err => res.status(500).send({ message: err.message }));
 };
 
 const getProfile = (req, res) => {
   return getDataFromFile(dataPath)
-    .then(users => users.find(user => user._id === req.params.id))
-    .then(user => {
+    .then(users => {
+      const user = users.find(user => user._id === req.params.id);
       if (!user) {
         return res.status(404).send({ message: 'Нет пользователя с таким id'});
       }
       res.status(200).send(user);
     })
-    .catch(err => res.status(404).send(err));
+    .catch(err => res.status(500).send({ message: err.message }));
 }
 
 module.exports = { getUsers, getProfile };
